32e4cd8a1bd0d05159a74f1282b2d28c13ddc05d
Richard Meissner <richard@gnosis.io>
2019-11-08 16:49:45 +0100

    added some play around code

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,53 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import PullToRefresh from 'rmc-pull-to-refresh';
 import logo from '../logo.svg';
 import './App.css';
 
+function ListItemLink(props: any) {
+  return <ListItem button component="a" {...props} />;
+}
+
 function App() {
+
+    const handleRefresh = async () => {
+      }
+
     return (
         <div className="App">
             <header className="App-header">
+                <PullToRefresh onRefresh={handleRefresh} >
+                    <List component="nav" aria-label="main mailbox folders">
+                        <ListItem button>
+                            <ListItemIcon>
+                            <InboxIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Inbox" />
+                        </ListItem>
+                        <ListItem button>
+                            <ListItemIcon>
+                            <DraftsIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Drafts" />
+                        </ListItem>
+                        </List>
+                        <Divider />
+                        <List component="nav" aria-label="secondary mailbox folders">
+                        <ListItem button>
+                            <ListItemText primary="Trash" />
+                        </ListItem>
+                        <ListItemLink href="#simple-list">
+                            <ListItemText primary="Spam" />
+                        </ListItemLink>
+                    </List>
+                </PullToRefresh>
                 <Button variant="contained" color="primary">
                     Hello World
                 </Button>
